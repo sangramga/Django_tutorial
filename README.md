48dc5a8bff23b6fd0e169d63592ede957bbe0e3c
sangramga <sangramga@gmail.com>
2018-03-24 12:58:59 +0530

    First Django App

---
+++ README.md
@@ -0,0 +1,5 @@
+# My First Django Project
+
+Polls app from official Django Tutorials
+Django version 2.0.3
+python version 3.6.4 Anaconda Distribution
---
+++ project/api/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+# Create your views here.
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+def index(request):
+    #return HttpResponse("Hellio you are inside api app views.py/index")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    # template = loader.get_template('api/index.html')
+    context = {
+            'latest_question_list':latest_question_list,
+    }
+    # response = template.render(context, request)
+    # response = ", ".join([q.q_text for q in latest_question_list])
+    # return HttpResponse(response)
+    return render(request, 'api/index.html', context)
+
+def detail(request, question_id):
+    # return HttpResponse("Detail view .. question_id {} ".format(question_id))
+    #try:
+    #    question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404("Question not found")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "api/details.html", {'question':question})
+
+
+def results(request, question_id):
+    
+    return HttpResponse(" Results View .. question_id %s " % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("VOting view ... question_id %s " % question_id)
+
