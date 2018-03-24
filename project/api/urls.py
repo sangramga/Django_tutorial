be87101d0820d2c065e87d634b06c852506ec801
sangramga <sangramga@gmail.com>
2018-03-24 15:45:43 +0530

    flake8 linting

--- project/api/urls.py
+++ project/api/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from . import views
-app_name = "api" #Set Namespace for Template using {% url %}
+app_name = "api"  # Set Namespace for Template using {% url %}
 urlpatterns = [
     path('', views.index, name="index"),
-    path('<int:question_id>/', views.detail, name ='detail'),
-    path('<int:question_id>/results', views.results, name = 'results'),
-    path('<int:question_id>/vote',views.vote, name = 'vote'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
-
