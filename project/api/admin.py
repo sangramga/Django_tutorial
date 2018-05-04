ff26686f262d894b8fdb68a03e7f75f07fc329a8
Sangram Gaikwad <sangramga@gmail.com>
2018-05-04 18:15:02 +0530

    Changes to admin Change List Page

--- project/api/admin.py
+++ project/api/admin.py
@@ -18,6 +18,25 @@ class QuestionAdmin(admin.ModelAdmin):
     # Add Choices to Questions Form
     inlines = [ChoiceInline]
 
+    # customize change list page in admin form -
+    # the one that displays all the questions in the system.
+    # By default Django only shows str() of object
+    list_display = ('q_text', 'pub_date', 'was_published_recently')
+
+    # add an improvement to the Question change list page:
+    # filters using the list_filter.
+    list_filter = ['pub_date']
+
+    """
+    Adds a search box at the top of the change list.
+    search for question_text field. Use as many fields as you’d like
+    – although because it uses a LIKE query behind the scenes,
+    limiting the number of search fields to a reasonable number.
+    """
+    search_fields = ['q_text']
+
+    """Number of objects per page of change list Default = 100"""
+    list_per_page = 10
 
 admin.site.register(Question, QuestionAdmin)
 # admin.site.register(Question)
--- project/api/models.py
+++ project/api/models.py
@@ -15,6 +15,11 @@ class Question(models.Model):
         return timezone.now() >= self.pub_date \
             >= timezone.now() - datetime.timedelta(days=1)
 
+    # For admin page management of list_display()
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     def __str__(self):
