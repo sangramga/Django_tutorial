6b200959561f1a7813dea6afb623afeda7474b80
Sangram Gaikwad <sangramga@gmail.com>
2018-05-10 18:04:02 +0530

    Modified some tests

--- project/api/tests.py
+++ project/api/tests.py
@@ -109,8 +109,8 @@ class QuestionDetailViewTests(TestCase):
         """
         future_question = create_question(q_text="Future question", days=5)
         url = reverse("api:detail", args=(future_question.id,))
-        response = self.client(url)
-        self.assertEqual(response.status_code, 404)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
     def test_past_question(self):
         """
@@ -119,5 +119,5 @@ class QuestionDetailViewTests(TestCase):
         """
         past_question = create_question("Past question", days=-5)
         url = reverse('api:detail', args=(past_question.id,))
-        response = self.client(url)
+        response = self.client.get(url)
         self.assertContains(response, past_question.q_text)
